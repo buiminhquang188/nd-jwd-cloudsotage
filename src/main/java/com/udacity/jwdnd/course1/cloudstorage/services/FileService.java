f22853e9da02b445de1f2db0ac550b2b3ac0cf01
Quang Bui <buiminhquang188@gmail.com>
2024-08-22 13:50:09 +0700

    Feat: Delete File

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/FileService.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/FileService.java
@@ -1,7 +1,13 @@
 package com.udacity.jwdnd.course1.cloudstorage.services;
 
+import com.udacity.jwdnd.course1.cloudstorage.dto.Response;
+import com.udacity.jwdnd.course1.cloudstorage.model.File;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface FileService {
     boolean upload(MultipartFile multipartFile, Integer userId);
+
+    File getFile(Integer fileId, Integer userId);
+
+    Response delete(Integer fileId, Integer userId);
 }
