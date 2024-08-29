e926a50a46bc05d5d6bc7a6adf9cb398633f9f79
Quang Bui <buiminhquang188@gmail.com>
2024-08-29 12:58:20 +0700

    Update: Formatted code & Refactor for using same WebDriver Config

--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -1,47 +1,24 @@
 package com.udacity.jwdnd.course1.cloudstorage;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
 
 import java.io.File;
 import java.time.Duration;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class CloudStorageApplicationTests {
-    @LocalServerPort
-    private int port;
-    private WebDriver driver;
-
-    @BeforeAll
-    static void beforeAll() {
-        WebDriverManager.chromedriver()
-                .setup();
-    }
-
-    @BeforeEach
-    public void beforeEach() {
-        this.driver = new ChromeDriver();
-    }
-
-    @AfterEach
-    public void afterEach() {
-        if (this.driver != null) {
-            driver.quit();
-        }
-    }
-
+class CloudStorageApplicationTests extends WebDriverConfig {
     @Test
     public void getLoginPage() {
-        driver.get("http://localhost:" + this.port + "/login");
-        Assertions.assertEquals("Login", driver.getTitle());
+        this.getDriver()
+                .get("http://localhost:" + this.getPort() + "/login");
+        Assertions.assertEquals("Login", this.getDriver()
+                .getTitle());
     }
 
     /**
@@ -53,34 +30,40 @@ class CloudStorageApplicationTests {
 
         // Visit the sign-up page.
 
-        WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(2));
-        driver.get("http://localhost:" + this.port + "/signup");
+        WebDriverWait webDriverWait = new WebDriverWait(this.getDriver(), Duration.ofSeconds(2));
+        this.getDriver()
+                .get("http://localhost:" + this.getPort() + "/signup");
         webDriverWait.until(ExpectedConditions.titleContains("Sign Up"));
 
         // Fill out credentials
         webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("inputFirstName")));
-        WebElement inputFirstName = driver.findElement(By.id("inputFirstName"));
+        WebElement inputFirstName = this.getDriver()
+                .findElement(By.id("inputFirstName"));
         inputFirstName.click();
         inputFirstName.sendKeys(firstName);
 
         webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("inputLastName")));
-        WebElement inputLastName = driver.findElement(By.id("inputLastName"));
+        WebElement inputLastName = this.getDriver()
+                .findElement(By.id("inputLastName"));
         inputLastName.click();
         inputLastName.sendKeys(lastName);
 
         webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("inputUsername")));
-        WebElement inputUsername = driver.findElement(By.id("inputUsername"));
+        WebElement inputUsername = this.getDriver()
+                .findElement(By.id("inputUsername"));
         inputUsername.click();
         inputUsername.sendKeys(userName);
 
         webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("inputPassword")));
-        WebElement inputPassword = driver.findElement(By.id("inputPassword"));
+        WebElement inputPassword = this.getDriver()
+                .findElement(By.id("inputPassword"));
         inputPassword.click();
         inputPassword.sendKeys(password);
 
         // Attempt to sign up.
         webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("buttonSignUp")));
-        WebElement buttonSignUp = driver.findElement(By.id("buttonSignUp"));
+        WebElement buttonSignUp = this.getDriver()
+                .findElement(By.id("buttonSignUp"));
         buttonSignUp.click();
 
         webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("success-msg")));
@@ -89,7 +72,8 @@ class CloudStorageApplicationTests {
 		// You may have to modify the element "success-msg" and the sign-up 
 		// success message below depening on the rest of your code.
 		*/
-        Assertions.assertTrue(driver.findElement(By.id("success-msg"))
+        Assertions.assertTrue(this.getDriver()
+                .findElement(By.id("success-msg"))
                 .getText()
                 .contains("You successfully signed up!"));
     }
@@ -101,21 +85,25 @@ class CloudStorageApplicationTests {
      **/
     private void doLogIn(String userName, String password) {
         // Log in to our dummy account.
-        driver.get("http://localhost:" + this.port + "/login");
-        WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        this.getDriver()
+                .get("http://localhost:" + this.getPort() + "/login");
+        WebDriverWait webDriverWait = new WebDriverWait(this.getDriver(), Duration.ofSeconds(2));
 
         webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("inputUsername")));
-        WebElement loginUserName = driver.findElement(By.id("inputUsername"));
+        WebElement loginUserName = this.getDriver()
+                .findElement(By.id("inputUsername"));
         loginUserName.click();
         loginUserName.sendKeys(userName);
 
         webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("inputPassword")));
-        WebElement loginPassword = driver.findElement(By.id("inputPassword"));
+        WebElement loginPassword = this.getDriver()
+                .findElement(By.id("inputPassword"));
         loginPassword.click();
         loginPassword.sendKeys(password);
 
         webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("login-button")));
-        WebElement loginButton = driver.findElement(By.id("login-button"));
+        WebElement loginButton = this.getDriver()
+                .findElement(By.id("login-button"));
         loginButton.click();
 
         webDriverWait.until(ExpectedConditions.titleContains("Home"));
@@ -138,11 +126,13 @@ class CloudStorageApplicationTests {
         // Create a test account
         doMockSignUp("Redirection", "Test", "RT", "123");
 
-        WebElement anchorLogin = driver.findElement(By.id("login-page"));
+        WebElement anchorLogin = this.getDriver()
+                .findElement(By.id("login-page"));
         anchorLogin.click();
 
         // Check if we have been redirected to the log in page.
-        Assertions.assertEquals("http://localhost:" + this.port + "/login", driver.getCurrentUrl());
+        Assertions.assertEquals("http://localhost:" + this.getPort() + "/login", this.getDriver()
+                .getCurrentUrl());
     }
 
     /**
@@ -164,8 +154,10 @@ class CloudStorageApplicationTests {
         doLogIn("UT", "123");
 
         // Try to access a random made-up URL.
-        driver.get("http://localhost:" + this.port + "/some-random-page");
-        Assertions.assertFalse(driver.getPageSource()
+        this.getDriver()
+                .get("http://localhost:" + this.getPort() + "/some-random-page");
+        Assertions.assertFalse(this.getDriver()
+                .getPageSource()
                 .contains("Whitelabel Error Page"));
     }
 
@@ -188,21 +180,24 @@ class CloudStorageApplicationTests {
         doLogIn("LFT", "123");
 
         // Try to upload an arbitrary large file
-        WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        WebDriverWait webDriverWait = new WebDriverWait(this.getDriver(), Duration.ofSeconds(2));
         String fileName = "upload5m.zip";
 
         webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("fileUpload")));
-        WebElement fileSelectButton = driver.findElement(By.id("fileUpload"));
+        WebElement fileSelectButton = this.getDriver()
+                .findElement(By.id("fileUpload"));
         fileSelectButton.sendKeys(new File(fileName).getAbsolutePath());
 
-        WebElement uploadButton = driver.findElement(By.id("uploadButton"));
+        WebElement uploadButton = this.getDriver()
+                .findElement(By.id("uploadButton"));
         uploadButton.click();
         try {
             webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.id("success")));
         } catch (org.openqa.selenium.TimeoutException e) {
             System.out.println("Large File upload failed");
         }
-        Assertions.assertFalse(driver.getPageSource()
+        Assertions.assertFalse(this.getDriver()
+                .getPageSource()
                 .contains("HTTP Status 403 – Forbidden"));
 
     }
---
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/WebDriverConfig.java
@@ -0,0 +1,49 @@
+package com.udacity.jwdnd.course1.cloudstorage;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.web.server.LocalServerPort;
+
+public class WebDriverConfig {
+    @LocalServerPort
+    private int port;
+    private WebDriver driver;
+
+    @BeforeAll
+    static void beforeAll() {
+        WebDriverManager.chromedriver()
+                .setup();
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        this.driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void afterEach() {
+        if (this.driver != null) {
+            driver.quit();
+        }
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(WebDriver driver) {
+        this.driver = driver;
+    }
+}
